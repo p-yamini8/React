c8cce504f6d18db9467a569e444fe06818848802
p-yamini8 <yaminifreez1@gmail.com>
2026-02-02 12:24:15 +0530

    added complex JSX using<> or <div>

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import './App.css'
 import Header from './Header'
 import Para from './paragraph'
+import ExpenseItems from './components/ExpenseItem'
 function App() {
   
 
   return (
     <>
+    <ExpenseItems></ExpenseItems>
     <Header/>
    <Para/>
     </>
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,14 @@
+function ExpenseItems()
+{
+  return (
+    <>
+      <div>15 August 2023
+        <div>Book
+        <div>$10
+        <div>Delhi</div></div></div>
+      </div>
+     
+    </>
+  )
+}
+export default ExpenseItems;
