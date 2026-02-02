96a422006e0d10d703577f024125cfeceda2fa99
p-yamini8 <yaminifreez1@gmail.com>
2026-02-02 14:46:09 +0530

    added styles

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,14 @@
+
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
   return (
     <div>
-      <div>15 August 2023</div>
+      <div className="expense-item">15 August 2023</div>
       <div>
-        <h2>Book</h2>
-        <div>$10</div>
+        <h2 className='expense-item__description'>Book</h2>
+        <div className='expense-item__price'>$10
+        <div className='expense-item__location'>Delhi</div></div>
       </div>
     </div>
   );
