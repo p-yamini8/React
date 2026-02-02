ac60fa2309fbf3ff0f240fd11bd4a6ab6deadc0a
p-yamini8 <yaminifreez1@gmail.com>
2026-02-02 15:33:43 +0530

    used props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
+// Write your code at relevant places in the code below:
 
-import './App.css'
-import Header from './Header'
-import Para from './paragraph'
-import ExpenseItems from './components/ExpenseItem'
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  
-
   return (
-    <>
-    <ExpenseItems></ExpenseItems>
-    <Header/>
-   <Para/>
-    </>
-  )
+    <div>
+      <h1>Let's get Started</h1>
+      <ExpenseItem
+        date={new Date(2023, 7, 15)}
+        title="Insurance"
+        price="50"
+        location="Bangalore"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        title="Book"
+        price="20"
+        location="Delhi"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        title="Pen"
+        price="5"
+        location="Hyderabad"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 14)}
+        title="Laptop"
+        price="200"
+        location="Mumbai"
+      ></ExpenseItem>
+    </div>
+  );
 }
 
-export default App
+export default App;
