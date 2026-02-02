9845bd261e08e6dcb78fca8932795facda3e129f
p-yamini8 <yaminifreez1@gmail.com>
2026-02-02 20:55:20 +0530

    formatting Date giving calender look

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,29 @@
+import './ExpenseItem.css'
+const month = ["January", "Feburary", "March", "April", "May", "June", "July", "Auguest", "September", "October", "November", "December"];
+function ExpenseItem(props)
+{
+  const date =String(props.date.getDate()).padStart(2,0);
+  const months = month[props.date.getMonth()];
+  const year = props.date.getFullYear();
 
-import "./ExpenseItem.css";
-
-function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__location">{props.location}</div>
+ <>
+      <div className="expense-item">
+        <div>
+        {months}
+        </div>
+        <div>{year}</div>
+      <div>{date}</div>
+    </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-          <div className="expense-item__price">${props.price}</div>
-       
+        <div className="expense-item__price">
+        ${props.price}
       </div>
-    </div>
-  );
-}
+      </div>
+    
+    </>
 
+  )
+}
 export default ExpenseItem;
