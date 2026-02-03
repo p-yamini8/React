9a8cbd02133feb8e2a4d03f29a9393617a09987a
p-yamini8 <yaminifreez1@gmail.com>
2026-02-03 15:08:23 +0530

    wrapped

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,29 +1,13 @@
-import './ExpenseItem.css'
-const month = ["January", "Feburary", "March", "April", "May", "June", "July", "Auguest", "September", "October", "November", "December"];
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from"./Card"
 function ExpenseItem(props)
 {
-  const date =String(props.date.getDate()).padStart(2,0);
-  const months = month[props.date.getMonth()];
-  const year = props.date.getFullYear();
-
   return (
- <>
-      <div className="expense-item">
-        <div>
-        {months}
-        </div>
-        <div>{year}</div>
-      <div>{date}</div>
-    </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">
-        ${props.price}
-      </div>
-      </div>
-    
-    </>
-
+    <h2>{props.title}</h2><div className="expense-item__price">${props.price}</div></div></Card>
   )
 }
 export default ExpenseItem;
