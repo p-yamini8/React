fef4a37e611b6dc146d79e71d27736c48cf786e6
p-yamini8 <yaminifreez1@gmail.com>
2026-02-27 10:37:42 +0530

    rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "../src/components/Expense/Expense";
 
 import NewExpense from "./components/newExpense/NewExpense"
 const App = () => {
-  const expenses = [
+  const initialExpenses = [
     { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
     { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
     { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
     { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
   ];
-  const addexpenseshandler = (addedexpenses) => {
+  const[expense,setExpenses]=useState(initialExpenses);
+  const addexpenseshandler = (expense) => {
+    setExpenses((prevExpenses)=>{
+return [expense,...prevExpenses]
+    })
   console.log(addedexpenses)
   }
 
   return (
     <div>
       <NewExpense onAddExpense={addexpenseshandler} />
-      <Expenses expenses={expenses} />
+      <Expenses expenses={initialExpenses} />
     </div>
   );
 };
